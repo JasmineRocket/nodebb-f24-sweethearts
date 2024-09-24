5209cf76030ea2ea3f711b4dcabd53cbfa2dce2b
Jasmine <151565574+JasmineRocket@users.noreply.github.com>
2024-09-23 21:15:01 -0400

    fixing grammar
    fixing trailing spaces and incorrect indentation

--- src/start.js
+++ src/start.js
@@ -7,20 +7,20 @@ const db = require('../database');
 const start = module.exports;
 
 // Function to add default tags
-module.exports = async function (Topics) {
+async function addDefaultTags(Topics) {
 	try {
 		// Define default tags
 		const Htag = ['Homework'];
-		const Htid = 0; 
-		const Htimestamp = Date.now(); 
+		const Htid = 0;
+		const Htimestamp = Date.now();
 		const Atag = ['Assignment'];
-		const Atid = 1; 
-		const Atimestamp = Date.now(); 
+		const Atid = 1;
+		const Atimestamp = Date.now();
 		
 		// Add the tags using the createTags function
 		await Topics.createTags(Htag, Htid, Htimestamp);
 		await Topics.createTags(Atag, Atid, Atimestamp);
-		
+
 		console.log(`Default tags added successfully.`);
 		} catch (error) {
 		console.error('Error adding default tags:', error);
