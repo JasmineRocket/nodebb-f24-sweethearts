117dd1e8f58024fd25fefb361f1cc49e8af5076e
jullia andrei <jullia@jullias-air.wifi.local.cmu.edu>
2024-10-08 21:47:53 -0400

    Fixing lint errors.

--- verifyUpgrade.js
+++ verifyUpgrade.js
@@ -4,27 +4,30 @@ const path = require('path');
 const nconf = require('nconf');
 
 nconf.argv().env({
-    separator: '__',
+	separator: '__',
 }).file({
-    file: path.join(__dirname, 'config.json'),
+	file: path.join(__dirname, 'config.json'),
 });
 
 async function verifyUpgrade() {
-    const db = require('./src/database');
-    await db.init();
-    
-    // Get 5 random user IDs
-    const uids = await db.getSortedSetRange('users:joindate', 0, 4);
-    
-    for (const uid of uids) {
-        const userSettings = await db.getObject(`user:${uid}:settings`);
-        console.log(`User ${uid} faculty reply setting:`, userSettings.notificationType_faculty_reply);
-    }
-    
-    process.exit();
+	const db = require('./src/database');
+	await db.init();
+
+	// Get 5 random user IDs
+	const uids = await db.getSortedSetRange('users:joindate', 0, 4);
+
+	// Process all user settings concurrently to avoid the 'no-await-in-loop' error
+	const userSettingsPromises = uids.map(async (uid) => {
+		const userSettings = await db.getObject(`user:${uid}:settings`);
+		console.log(`User ${uid} faculty reply setting:`, userSettings.notificationType_faculty_reply);
+	});
+
+	await Promise.all(userSettingsPromises); // Await all promises together
+
+	process.exit();
 }
 
 verifyUpgrade().catch((err) => {
-    console.error('Error during verification:', err);
-    process.exit(1);
+	console.error('Error during verification:', err);
+	process.exit(1);
 });
